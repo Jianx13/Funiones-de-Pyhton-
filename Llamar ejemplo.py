31a1436f9b6a47be861a01ff4b6918b117215ac7
Kevin Jimenez <159860152+Jianx13@users.noreply.github.com>
2026-04-07 08:57:04 -0500

    Add files via upload

---
+++ Llamar ejemplo.py
@@ -0,0 +1,10 @@
+# llamar_ejemplo.py
+
+import funciones_ejemplo
+
+def demostrar_todas():
+    print("Resultado abs:", funciones_ejemplo.ejemplo_abs())
+    print("Suma:", funciones_ejemplo.ejemplo_suma(5, 3))
+    print("Multiplicación:", funciones_ejemplo.ejemplo_multiplicacion(4, 2))
+
+demostrar_todas()
---
+++ funciones_ejemplo.py
@@ -0,0 +1,10 @@
+# funciones_ejemplo.py
+
+def ejemplo_abs():
+    return abs(-10)
+
+def ejemplo_suma(a, b):
+    return a + b
+
+def ejemplo_multiplicacion(a, b):
+    return a * b
